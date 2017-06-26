038b7b7400a71d201d1ead6127439802e3607d76
Inge Wieser <ingewieser@gmail.com>
2017-06-25 21:10:15 -0700

    Add Sign In functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
-  get 'hello_world', to: 'hello_world#index'
+  # get 'hello_world', to: 'hello_world#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:new, :create, :edit, :update, :show, :destroy]
 
   get '/', to: 'welcome#index', as: 'home'
+  get '/about', to: 'welcome#about'
+  get '/map', to: 'welcome#map'
+
+  resources :sessions, only: [:new, :create] do
+    get :destroy, on: :collection
+  end
 
 
 end
