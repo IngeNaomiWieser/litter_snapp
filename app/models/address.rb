c8fc840679d7e3284f52eec9e17b41a06729f9ed
Inge Wieser <ingewieser@gmail.com>
2017-06-26 22:54:47 -0700

    Add functionality to geocode user address on sign up

--- app/models/address.rb
+++ app/models/address.rb
@@ -5,4 +5,25 @@ class Address < ApplicationRecord
   # validates :city, presence: true
   validates :country, presence: true
 
+  geocoded_by :full_street_address   # can also be an IP address
+  after_validation :geocode          # auto-fetch coordinates
+
+  def full_street_address
+    [self.line_1, self.city, self.postal_code, self.province, self.country].reject(&:blank?).join(", ")
+  end
+
+  def geocode
+    address = self.full_street_address
+    result = Geocoder.search(address).first
+
+    process_geocoded_result(result) if result
+  end
+
+  def process_geocoded_result(result)
+    location = Location.new user_id: self.user.id  #create location and assign it to user that belongs to that address
+    location.latitude = result.latitude
+    location.longitude = result.longitude
+    location.save
+  end
+
 end
