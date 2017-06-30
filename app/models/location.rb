772dc59a2a34aa78cda63ecc115262d1f6b1131b
Inge Wieser <ingewieser@gmail.com>
2017-06-29 20:24:02 -0700

    Show location user on map

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,5 +1,5 @@
 class Location < ApplicationRecord
   belongs_to :litter, optional: true
   belongs_to :user, optional: true
-  belongs_to :event, optional: true 
+  belongs_to :event, optional: true
 end
