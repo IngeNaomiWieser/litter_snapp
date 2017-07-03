2596870afd7a1149dffd4009b6773531372e8f03
Tim Eijgelshoven <teijgelshoven@rennie.com>
2017-07-02 22:13:40 -0700

    Setup for react component on map page

--- client/app/bundles/LitterSnapp/components/MapSideBar.jsx
+++ client/app/bundles/LitterSnapp/components/MapSideBar.jsx
@@ -2,19 +2,16 @@ import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 
 export default class MapSideBar extends Component {
-  // static propTypes = {
-  //
-  // };
-  //
-  // constructor(props, _railsContext) {
-  //   super(props);
-  //
-  // }
-
   render() {
     return (
-      <div className='MapSideBar'>
-        Hi, I'm going to be a full fledged side bar
+      <div className='mapsidebar'>
+        <ul className='mapsidebar-tabs'>
+          <li>Events</li>
+          <li>Organize</li>
+          <li>Past</li>
+        </ul>
+        <p>Just some sidebar for now...</p>
+
       </div>
     );
   }
--- client/app/bundles/LitterSnapp/startup/registration.jsx
+++ client/app/bundles/LitterSnapp/startup/registration.jsx
@@ -1,6 +1,6 @@
 import ReactOnRails from 'react-on-rails';
 
-import HelloWorld from '../components/HelloWorld';
+import HelloWorldApp from './HelloWorldApp';
 import MapSideBar from '../components/MapSideBar';
 
 // This is how react_on_rails can see the HelloWorld in the browser.
