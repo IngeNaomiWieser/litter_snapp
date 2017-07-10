2e7764f4718174087aa7cc2f31755ebc5336104a
Inge Wieser <ingewieser@gmail.com>
2017-07-09 19:02:55 -0700

    Implement react on map page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
-  
+
+  include ActionView::Helpers::AssetUrlHelper
+
   def index
   end
 
@@ -11,5 +13,48 @@ class WelcomeController < ApplicationController
     #have a function that checks if a user is logged in. If yes, it sets an instance variable to the location (lat and long) of the user.
     # then in your view file you will have to add the markers to the instance variable
     @litters = Litter.where(cleaned: false)
+    @map_props = map_props
   end
+
+  private
+
+  def map_props
+    {
+      litters: litter_props(Litter.where(cleaned: false)),
+      tab: 'index',
+      user_location: current_user&.location,
+      tab: 'index',
+      planned_events: event_list(
+        Event.where('planned_date >= ?', Date.today).order(planned_date: :asc).to_a
+      ),
+      past_events: event_list(
+        Event.where('planned_date < ?', Date.today).order(planned_date: :desc).to_a
+      )
+    }
+  end
+
+  def event_list(events) #takes array of event objects
+    events.map{|e| {
+        id: e.id,
+        planned_date: e.planned_date,
+        planned_time: e.planned_time.strftime("%I:%M %p"),
+        lat: e.location&.latitude,
+        lng: e.location&.longitude,
+        url: event_path(e),
+        joined: e.users.count
+      }
+    }
+  end
+
+  def litter_props(litters)
+    litters.map{|l| {
+        id: l.id,
+        lat: l.location.latitude,
+        lng: l.location.longitude,
+        img: l.image_url(:medium),
+        amount: l.amount
+      }
+    }
+  end
+
 end
---
+++ client/app/bundles/LitterSnapp/components/MapSideBar.jsx
@@ -0,0 +1,51 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import EventForm from './EventForm';
+import EventList from './EventList';
+
+export default class MapSideBar extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div className='mapsidebar'>
+        <ul className='mapsidebar-tabs'>
+          <li
+            className={this.props.tab == 'index' ? 'active-item' : ''}
+            onClick={(e) => this.props.updateTab('index')}
+          >Planned</li>
+          <li
+            className={this.props.tab == 'create' ? 'active-item' : ''}
+            onClick={(e) => this.props.updateTab('create')}
+          >Organize</li>
+          <li
+            className={this.props.tab == 'past_index' ? 'active-item' : ''}
+            onClick={(e) => this.props.updateTab('past_index')}
+          >Past</li>
+        </ul>
+        { this.props.tab == 'past_index' &&
+          <EventList events={this.props.past_events} tab={this.props.tab} google_map={this.props.google_map} />
+        }
+        { this.props.tab == 'create' &&
+          <EventForm google_map={this.props.google_map} clickLatLng={this.props.clickLatLng} />
+        }
+        { this.props.tab == 'index' &&
+          <EventList events={this.props.planned_events} tab={this.props.tab} google_map={this.props.google_map} />
+        }
+      </div>
+    )
+  }
+}
+
+MapSideBar.propTypes = {
+  tab: PropTypes.string.isRequired,
+  updateTab: PropTypes.func.isRequired,
+  planned_events: PropTypes.array.isRequired,
+  past_events: PropTypes.array.isRequired,
+  google_map: PropTypes.object,
+  clickLatLng: PropTypes.object
+};
+
+// export default MapSideBar;
